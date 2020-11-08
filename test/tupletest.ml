5d62904dd13702ffadbd9522829a671fedef708a
Kendall Yu <yutienmu@gmail.com>
2020-11-07 19:04:22 -0500

    fixed merge conflict

--- test/tupletest.ml
+++ test/tupletest.ml
@@ -163,48 +163,6 @@ let tests = "Test Suite for Tuple" >::: [
         assert_bool "v2 x v1 failed" 
             (equalTuple (cross v2 v1) v2_cross_v1);
     );
-<<<<<<< HEAD
-=======
-    "Color constructor" >::
-    (fun _ ->
-        let c = color (-0.5) 0.4 1.7 in
-	assert_equal c.red (-0.5);
-	assert_equal c.green 0.4;
-	assert_equal c.blue 1.7;
-
-    );
-    "Adding colors" >::
-    (fun _ ->
-        let c1 = color 0.9 0.6 0.75 in
-	let c2 = color 0.7 0.1 0.25 in
-	let expected_sum = color 1.6 0.7 1.0 in
-	assert_bool "color addition incorrect"
-	    (equalColor (addColor c1 c2) expected_sum);
-    );
-    "Subtracting colors" >::
-    (fun _ ->
-        let c1 = color 0.9 0.6 0.75 in
-	let c2 = color 0.7 0.1 0.25 in
-	let expected_diff = color 0.2 0.5 0.5 in
-	assert_bool "color subtraction incorrect"
-	    (equalColor (subtractColor c1 c2) expected_diff);
-    );
-    "Scalar color multiplication" >::
-    (fun _ ->
-        let c1 = color 0.2 0.3 0.4 in
-	let expected_product = color 0.4 0.6 0.8 in
-	assert_bool "color scalar multiplication incorrect"
-	    (equalColor (multiplyColorScalar c1 2.) expected_product);
-    );
-    "Multiplying colors" >::
-    (fun _ ->
-        let c1 = color 1. 0.2 0.4 in
-	let c2 = color 0.9 1. 0.1 in
-	let expected_product = color 0.9 0.2 0.04 in
-	assert_bool "color multiplication incorrect"
-	    (equalColor (multiplyColor c1 c2) expected_product);
-    );
->>>>>>> 095fdf60103d5f597557472f1ba214254b1b3eb4
 ]
 
 let _ = run_test_tt_main tests
