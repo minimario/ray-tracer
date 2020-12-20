08a50ace0ad76e9985e269c4d1e0eda6b0ad1975
Alex Gu <gua@mit.edu>
2020-12-19 22:10:15 -0500

    finished section on building a world (up to pg 200)

--- src/intersections.ml
+++ src/intersections.ml
@@ -1,11 +1,12 @@
 type intersection = {t: float; intersection_object: Sphere.shape}
 let intersection t intersection_object = {t; intersection_object}
 
+let cmp x y = 
+    if (Tuple.float_equals x.t y.t) then 0
+    else if (x.t < y.t) then (-1)
+    else 1
+
 let hit intersection_list  = 
-    let cmp x y = 
-        if (Tuple.float_equals x.t y.t) then 0
-        else if (x.t < y.t) then (-1)
-        else 1 in
     let positive_intersections = (List.filter (fun x->x.t >= 0.) intersection_list) in
     let sorted_intersections = 
         List.sort cmp positive_intersections
--- test/worldtest.ml
+++ test/worldtest.ml
@@ -7,6 +7,7 @@ let tests = "Test Suite for World" >::: [
 	assert_bool "should have no objects" ((List.length w.objects) == 0);
 	assert_bool "should have no light source" (Option.is_none w.light);
     );
+
     "The default world" >::
     (fun _ ->
     	let light = Reflection.point_light (Tuple.point ~-.10. 10. ~-.10.) (Color.color 1. 1. 1.) 
@@ -14,16 +15,17 @@ let tests = "Test Suite for World" >::: [
 	assert_bool "incorrect light source" (Reflection.point_light_equals (Option.get w.light) light);
 
 	let s1 = Sphere.sphere
-    	and (material: Reflection.material) = {color=Color.color 1. 1. 1.; ambient=0.1; diffuse=0.9;
-                   specular=0.9; shininess=200.0} in
-    	Sphere.set_material s1 material;
-	assert_bool "outer sphere not found" (World.contains w s1);
+    	and (material: Reflection.material) = {color=Color.color 0.8 1.0 0.6; ambient=0.1; diffuse=0.7;
+                   specular=0.2; shininess=200.0} in
+    	let s1' = Sphere.set_material s1 material in
+	assert_bool "outer sphere not found" (World.contains w s1');
 
     	let s2 = Sphere.sphere
     	and new_transform = Transformations.scaling 0.5 0.5 0.5 in
-    	Sphere.set_transform s2 new_transform;
-	assert_bool "inner sphere not found" (World.contains w s2);
+    	let s2' = Sphere.set_transform s2 new_transform in
+	assert_bool "inner sphere not found" (World.contains w s2');
     );
+
     "Precomputing the state of an intersection" >::
     (fun _ ->
 	let shape = Sphere.sphere in
@@ -39,6 +41,98 @@ let tests = "Test Suite for World" >::: [
 	assert_bool "precomputed normalv incorrect"
 	    (Tuple.equals comps.normalv (Tuple.vector 0. 0. ~-.1.));
     );
-]
+
+	"Intersect a world with a ray" >::
+	(fun _ ->
+		let w = World.default_world in
+		let r = Rays.ray (Tuple.point 0. 0. (-5.)) (Tuple.vector 0. 0. 1.) in 
+		let xs = World.intersect_world w r in 
+		match xs with 
+		| i1 :: i2 :: i3 :: i4 :: [] -> 
+			assert ((Tuple.float_equals i1.t 4.) && (Tuple.float_equals i2.t 4.5) &&
+				   (Tuple.float_equals i3.t 5.5) && (Tuple.float_equals i4.t 6.))
+		| _ -> assert false
+	);
+
+	"Hit when intersection is on outside" >::
+	(fun _ ->
+		let r = Rays.ray (Tuple.point 0. 0. (-5.)) (Tuple.vector 0. 0. 1.) in
+		let shape = Sphere.sphere in
+		let i = Intersections.intersection 4. shape in
+		let comps = World.prepare_computations i r in
+		assert (not comps.inside)
+	);
+
+	"Hit when intersection is on inside" >::
+	(fun _ ->
+		let r = Rays.ray (Tuple.point 0. 0. 0.) (Tuple.vector 0. 0. 1.) in
+		let shape = Sphere.sphere in
+		let i = Intersections.intersection 1. shape in
+		let comps = World.prepare_computations i r in
+		assert (Tuple.equals comps.point (Tuple.point 0. 0. 1.));
+		assert (Tuple.equals comps.eyev (Tuple.vector 0. 0. (-1.)));
+		assert comps.inside;
+		assert (Tuple.equals comps.normalv (Tuple.vector 0. 0. (-1.)));
+	);
+
+	"Shading an intersection" >::
+	(fun _ ->
+		let w = World.default_world in
+		let r = Rays.ray (Tuple.point 0. 0. (-5.)) (Tuple.vector 0. 0. 1.) in
+		let shape = List.nth w.objects 0 in
+		let i = Intersections.intersection 4. shape in 
+		let comps = World.prepare_computations i r in
+		let c = World.shade_hit w comps in
+		assert (Color.equals c (Color.color 0.38066 0.47583 0.2855))
+	);
+
+	"Shading an intersection from the inside" >::
+	(fun _ ->
+		let world_light = Reflection.point_light (Tuple.point 0. 0.25 0.) (Color.color 1. 1. 1.) in
+		let w = {World.default_world with light= Some world_light} in
+		let r = Rays.ray (Tuple.point 0. 0. 0.) (Tuple.vector 0. 0. 1.) in
+		let shape = List.nth w.objects 1 in
+		let i = Intersections.intersection 0.5 shape in 
+		let comps = World.prepare_computations i r in
+		let c = World.shade_hit w comps in
+		assert (Color.equals c (Color.color 0.90498 0.90498 0.90498))
+	);
+
+	"The colour when a ray misses" >::
+	(fun _ ->
+		let w = World.default_world in
+		let r = Rays.ray (Tuple.point 0. 0. (-5.)) (Tuple.vector 0. 1. 0.) in
+		let c = World.color_at w r in
+		assert (Color.equals c Color.black)
+	);
+
+	"The colour when a ray hits" >::
+	(fun _ ->
+		let w = World.default_world in
+		let r = Rays.ray (Tuple.point 0. 0. (-5.)) (Tuple.vector 0. 0. 1.) in
+		let c = World.color_at w r in
+		assert (Color.equals c (Color.color 0.38066 0.47583 0.2855))
+	);
+
+	"The colour when a ray hits" >::
+	(fun _ ->
+		let w = World.default_world in
+		let r = Rays.ray (Tuple.point 0. 0. (-5.)) (Tuple.vector 0. 0. 1.) in
+		let c = World.color_at w r in
+		assert (Color.equals c (Color.color 0.38066 0.47583 0.2855))
+	);
+
+	"The colour with an intersection behind the ray" >::
+	(fun _ ->
+		let w = World.default_world in
+		let outer = List.nth w.objects 0 in
+		let new_outer = {outer with material={outer.material with ambient=1.}} in 
+		let inner = List.nth w.objects 1 in
+		let new_inner = {inner with material={inner.material with ambient=1.}} in 
+		let r = Rays.ray (Tuple.point 0. 0. 0.75) (Tuple.vector 0. 0. (-1.)) in
+		let c = World.color_at {w with objects=[new_outer; new_inner]} r in
+		assert (Color.equals c inner.material.color)
+	);
+]	
 
 let _ = run_test_tt_main tests
